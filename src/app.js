bd918daebe5aeb61ba37bc08237c531ba89439ce
Juanita Soranno <juanitasoranno@gmail.com>
2017-01-18 22:18:15 -0500

    changed import react-router

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@
  import React from 'react';
  import ReactDOM from 'react-dom';
  import routes from './config/routes';
- import { IndexRoute, Route, browserHistory} from 'react-router';
+ //import { IndexRoute, Route, browserHistory} from 'react-router';
  import { AxiosProvider, Request, Get, Delete, Head, Post, Put, Patch } from 'react-axios'
  var Router = require('react-router').Router;
 
