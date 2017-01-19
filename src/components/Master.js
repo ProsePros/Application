9cc4550348b4dc4f9e7cd7cf1dbe15f685053a01
Juanita Soranno <juanitasoranno@gmail.com>
2017-01-18 23:40:55 -0500

    added Navbar component

--- src/components/Master.js
+++ src/components/Master.js
@@ -1,3 +1,13 @@
 /**
  * Created by juanitasoranno on 1/18/17.
  */
+*/
+import React from 'react';
+
+export class Master extends React.Component {
+    render() {
+        return (
+
+        );
+    }
+}
