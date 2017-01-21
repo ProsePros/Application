82859ffee79189ace5e7b2f759237b669f581408
Juanita Soranno <juanitasoranno@gmail.com>
2017-01-21 10:38:19 -0500

    Reviewed components for bugs - found and changed

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,10 +2,17 @@
  * Created by juanitasoranno on 1/18/17.
  */
 // Dependencies
-import React from 'react';
-var Router = require('react-router');
-var Route = Router.Route;
+var React = require('react');
+var router = require('react-router');
+var Route = router.Route;
+var IndexRoute = router.IndexRoute;
+var Router = router.Router;
+var browserHistory = router.browserHistory;
 
+//Why must we change all of the imports? It works on the other tutorials using nodeJS T_T
+//Am I supposed to have an index.js and this routes.js separated? Just listing and exporting the routes, then rendering from the other file?
+//Why can't I test this without internet???????
+//this could be why https://stackoverflow.com/questions/33950433/warning-failed-proptype-invalid-prop-component-supplied-to-route
 
 //Components
 var Master = require('../components/Master');
@@ -16,22 +23,9 @@ var Navbar = require('../components/Navbar');
 var Footer = require('../components/Footer');
 
 
-//
-// render(
-//     <Router history={browserHistory} routes={routes} />,
-//     document.getElementById('app')
-// );
-//
-// module.exports = (
-//     <Route path="/" component={Master}>
-//         <Route path='Original' component={Original} />
-//         <Route path='Revision' component={Revision} />
-//     </Route>
-// );
 
 
-
-render(
+module.exports = (
     <Router history={browserHistory}>
         <Route path='/' component={Master}>
             <Route path='nav' component={Navbar} />
@@ -39,6 +33,7 @@ render(
             <Route path='original' component={Original} />
             <Route path='revision' component={Revision} />
             <Route path='footer' component={Footer} />
+            <IndexRoute component={Master} />
         </Route>
     </Router>,
     document.getElementById('app')
